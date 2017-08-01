e1ef86c9a6f1d2be4653174093dce495083a6dec
Dan Stechman <stechma2@illinois.edu>
2017-08-01 14:58:49 -0500

    Modified PECAN sizeDist wrapper to work properly with parent python wrapper.

--- runSizeDistPECAN.m
+++ runSizeDistPECAN.m
@@ -1,52 +1,63 @@
-function runSizeDistPECAN(ddate)
+function runSizeDistPECAN(ddate,runCIP,runPIP)
+	
+	% Added this line as MATLAB would not ls properly in -nodisplay mode without it...
+	cd /data/pecan/a/stechma2/pecan/mp-data/UIOPS
+	
+	%runCIP = 1;
+	%runPIP = 1;
+	
+	projectname = 'PECAN';
+		
+	datadate=num2str(ddate);
+	
+	prmsF = ['/data/pecan/a/stechma2/pecan/' datadate '_PECANparams.nc'];
+	
+	%% Run CIP
+	if runCIP
+		clearvars -except ddate runCIP runPIP projectname datadate prmsF
 
-rCIP = 0;
+		tasfilename =  ls('--color=none',['../' num2str(ddate) '/01CIP*.csv']);
+		dirpath = pwd;
+		cipTASf = strtrim([dirpath '/' tasfilename]);
+		loadCIPcsv
+		
+		tas = CIP_True_Air_Speed;
+		timehhmmss=insec2hhmmss(CIP_Time);
+		pres=CIP_Static_Press+CIP_Diff_Press;
+		temp1=CIP_Ambient_Temp;
+		
+		probe='CIP';
+		
+		% Specify the input filename, output filename, true air speed, time (hhmmss), probe name,
+		% Dmax definition, surface area method, pressure and temperature
+		outFile = ['sdistCI.' datadate '.' probe '.cdf']; % Uncompressed netCDF file name
+		inFile =  ['proc2.' datadate '.' probe '.cdf'];
+		
+		sizeDist(inFile,outFile, tas, floor(timehhmmss),probe, 6, 0, pres, temp1, projectname, datadate, 'NONE', prmsF);
+	end
+	
+	%% Run PIP
+	if runPIP
+		clearvars -except ddate runCIP runPIP projectname datadate prmsF
 
-%% Run CIP
-if rCIP == 1 
-clearvars -except ddate
-inst=num2str(ddate);
-%inst = '20150617';
-tasfilename =  '01CIP20150702015858.csv'; %ls('--color=none',['../PECAN/' num2str(ddate) '/01CIP*.csv']);
-dirpath = pwd;
-tasfilename = strtrim([dirpath '/' tasfilename]);
-loadTASinfo
-
-tas = True_Air_Speed;
-timehhmmss=insec2hhmmss(Time);
-pres=Static_Press+Diff_Press;
-temp1=Ambient_Temp;
-
-probe='CIP';
-% Specify the desination file name infilename and diodesize ds in millimeters
-outFile = ['sdistCI.' inst '.' probe '.cdf'];                  	% Input uncompressed netCDF file name
-% Output file name
-inFile =  ['proc2.' inst '.' probe '.cdf'];
-inst
-sizeDist(inFile,outFile, tas, floor(timehhmmss),probe, 6, 0, pres, temp1);
+		inst=num2str(ddate);
+		tasfilename = ls('--color=none',['../' num2str(ddate) '/00PIP*.csv']); %'00PIP20150617005743.csv';
+		dirpath = pwd;
+		pipTASf = strtrim([dirpath '/' tasfilename]);
+		loadPIPcsv
+		
+		tas = PIP_True_Air_Speed;
+		timehhmmss=insec2hhmmss(PIP_Time);
+		pres=PIP_Static_Press+PIP_Diff_Press;
+		temp1=PIP_Ambient_Temp;
+		
+		probe='PIP';
+		
+		% Specify the input filename, output filename, true air speed, time (hhmmss), probe name,
+		% Dmax definition, surface area method, pressure and temperature
+		outFile = ['sdistCI.' inst '.' probe '.cdf'];                  	% Input uncompressed netCDF file name
+		inFile =  ['proc2.' inst '.' probe '.cdf'];
+		
+		sizeDist(inFile,outFile, tas, floor(timehhmmss),probe, 6, 0, pres, temp1, projectname, datadate, 'NONE', prmsF);
+	end
 end
-%% Run PIP
-clearvars -except ddate
-inst=num2str(ddate);
-%inst = '20150617';
-tasfilename = '00PIP20150620005009.csv'; %ls('--color=none',['../PECAN/' num2str(ddate) '/00PIP*.csv']); %'00PIP20150617005743.csv';
-dirpath = pwd;
-tasfilename = strtrim([dirpath '/' tasfilename]);
-loadTASinfo
-
-tas = True_Air_Speed;
-timehhmmss=insec2hhmmss(Time);
-pres=Static_Press+Diff_Press;
-temp1=Ambient_Temp;
-
-probe='PIP';
-% Specify the desination file name infilename and diodesize ds in millimeters
-outFile = ['sdistCI.' inst '.' probe '.cdf'];                  	% Input uncompressed netCDF file name
-% Output file name
-inFile =  ['proc2.TEST.' probe '.cdf'];
-
-sizeDist(inFile,outFile, tas, floor(timehhmmss),probe, 6, 0, pres, temp1,'PECAN1',ddate);
-
-
-end
-
